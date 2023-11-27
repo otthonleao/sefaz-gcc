12cf5b7b972537269f902f75c37ac4256eba189b
Otthon Leão <otthonleao@hotmail.com>
2023-11-27 10:48:00 -0400

    first commit

---
+++ src/main/java/dev/otthon/sefaz/gcc/GccApplication.java
@@ -0,0 +1,13 @@
+package dev.otthon.sefaz.gcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GccApplication.class, args);
+	}
+
+}
